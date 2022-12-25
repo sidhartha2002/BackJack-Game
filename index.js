239d4b34e99194a7f5323607ffe405f3424a69a2
Sidhartha Mohanty <mohantysidhartha2002@gmail.com>
2022-12-26 02:12:25 +0530

    new card game upload

---
+++ index.js
@@ -0,0 +1,63 @@
+let cards = [];
+// let n = cards.length;
+let sum = 0;
+let hasBlackJack = false;
+let isAlive = false;
+let message = "";
+
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardEl = document.querySelector("#card-el");
+
+let player = { name: "AgentSID", chips: 145 };
+let playerEl = document.getElementById("player-el");
+playerEl.textContent = player.name + ": $" + player.chips;
+function getRandomCard() {
+  let randomNumber = Math.floor(Math.random() * 13) + 1;
+  if (randomNumber > 10) {
+    return 10;
+  } else if (randomNumber === 1) {
+    return 11;
+  } else {
+    return randomNumber;
+  }
+}
+
+function startGame() {
+  isAlive = true;
+  let firstCard = getRandomCard();
+  let secondCard = getRandomCard();
+  cards = [firstCard, secondCard];
+  sum = firstCard + secondCard;
+
+  renderGame();
+}
+
+function renderGame() {
+  cardEl.textContent = "Cards: ";
+
+  for (let i = 0; i < cards.length; i += 1) {
+    cardEl.textContent += cards[i] + " ";
+  }
+  sumEl.textContent = "Sum: " + sum;
+
+  if (sum <= 20) {
+    message = "Do you want to draw a new card? 🙂";
+  } else if (sum === 21) {
+    message = "Wohoo! You've got Blackjack! 🥳";
+    hasBlackJack = true;
+  } else {
+    message = "You're out of the game! 😭";
+    isAlive = false;
+  }
+  messageEl.textContent = message;
+}
+
+function newCard() {
+  if (isAlive === true && hasBlackJack === false) {
+    let card_new = getRandomCard();
+    sum += card_new;
+    cards.push(card_new);
+    renderGame();
+  }
+}
